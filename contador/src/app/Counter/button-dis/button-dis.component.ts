eb934250c853b838bc56b0357afe10b8737ddd42
Marco <126091311+Zawabanas@users.noreply.github.com>
2024-03-18 20:07:28 -0600

    Contador_2

---
+++ contador/src/app/Counter/button-dis/button-dis.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button-dis',
+  templateUrl: './button-dis.component.html',
+  styleUrl: './button-dis.component.css'
+})
+export class ButtonDisComponent {
+
+  @Input()
+  contador: number = 0;
+
+  @Output() 
+  onDis: EventEmitter<number> = new EventEmitter();
+
+  Dis(){
+    this.contador--;
+    this.onDis.emit(this.contador);
+  }
+
+}
--- contador/src/app/app.module.ts
+++ contador/src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './Counter/counter.component';
+import { ButtonAddComponent } from './Counter/button-add/button-add.component';
+import { ButtonDisComponent } from './counter/button-dis/button-dis.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CounterComponent, ButtonAddComponent, ButtonDisComponent],
+  imports: [BrowserModule,AppRoutingModule],
   providers: [
     provideClientHydration()
   ],
